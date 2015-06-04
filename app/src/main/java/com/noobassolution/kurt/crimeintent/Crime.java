554b71ec2f8e6abd7213b21a6763268cc1b81b84
kurt <a.kapustkin@gmail.com>
2015-06-05 00:06:56 +0300

    Rejected commit: Default

---
+++ app/src/main/java/com/noobassolution/kurt/crimeintent/Crime.java
@@ -0,0 +1,25 @@
+package com.noobassolution.kurt.crimeintent;
+
+import java.util.UUID;
+
+public class Crime {
+
+    private UUID mId;
+    private String mTitle;
+
+    public Crime() {
+        mId = UUID.randomUUID();
+    }
+
+    public UUID getId() {
+        return mId;
+    }
+
+    public String getTitle() {
+        return mTitle;
+    }
+
+    public void setTitle(String mTitle) {
+        this.mTitle = mTitle;
+    }
+}
